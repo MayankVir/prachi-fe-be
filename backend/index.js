d0857a0d36815bc59920f14ea274f73adbc1f55f
MayankVir <virmanimayank15@gmail.com>
2024-02-20 02:18:13 +0530

    inital commit

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+
+app.use(
+  cors({
+    method: "*",
+    origin: "*",
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Hi, we have received a message");
+});
+
+app.post("/submitForm", (req, res) => {
+  // First request m se data nikalo for name and email
+  const body = req.body;
+  console.log({ body });
+  // Store it in the db
+  data.push(body);
+  // Send back the updated data
+  res.send({ data });
+});
+
+app.listen(8080, () => {
+  console.log("listening to port 8080");
+});
+
+// GET -> BASE_URL, HEADERS
+// POST -> ENDPOINT,  DATA,HEADERS
+
+let data = [];
---
+++ frontend/script.js
@@ -0,0 +1,54 @@
+// Multiple ways to target any html element
+// const nameInput2 = document.getElementsByClassName("nameInputClass");
+// const nameInput3 = document.querySelector("#nameInput");
+// const nameInput4 = document.querySelector(".nameInputClass");
+
+const BASE_URL = "http://localhost:8080/";
+
+const nameInput = document.getElementById("nameInput");
+const emailInput = document.getElementById("emailInput");
+const submitButton = document.getElementById("submitButton");
+
+submitButton.onclick = async () => {
+  console.log("submitButton clicked");
+  const nameInputValue = nameInput.value;
+  const emailInputValue = emailInput.value;
+  console.log({ nameInputValue, emailInputValue });
+
+  // Need to hit the POST route
+  const response = await axios.post(
+    BASE_URL + "submitForm",
+    { name: nameInputValue, email: emailInputValue },
+    {
+      "Access-Control-Allow-Origin": "*",
+    }
+  );
+
+  const data = response.data.data;
+  const tableElement = document.getElementById("table");
+
+  const newTr = document.createElement("tr");
+
+  const nameTh = document.createElement("th");
+  const emailTh = document.createElement("th");
+  nameTh.innerText = "Name";
+  emailTh.innerText = "Email";
+  newTr.appendChild(nameTh);
+  newTr.appendChild(emailTh);
+
+  tableElement.appendChild(newTr);
+
+  for (let i = 0; i < data.length; i++) {
+    const newTr = document.createElement("tr");
+
+    const nameTd = document.createElement("td");
+    const emailTd = document.createElement("td");
+    nameTd.innerText = data[i].name;
+    emailTd.innerText = data[i].email;
+
+    newTr.appendChild(nameTd);
+    newTr.appendChild(emailTd);
+
+    tableElement.appendChild(newTr);
+  }
+};
